c5df93d54ce5127540d3e8be38176ad40d238d2a
Balmasexy <www.alabamaabiodun@gmail.com>
2026-07-24 03:08:30 +0100

    fix: repair prisma configuration and restore savings module

--- apps/backend/prisma.config.ts
+++ apps/backend/prisma.config.ts
@@ -3,10 +3,8 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
+
   datasource: {
-    url: process.env.DATABASE_postgresql://pwfb_microfinance_postgre_user:5AJvpBOpvE48N9iupH9Ej2cJaVrlSKA0@dpg-d9f2q8rtqb8s73bgc0f0-a/pwfb.
+    url: process.env.DATABASE_URL!,
   },
 });

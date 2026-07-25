8be2095bd19fee1955e7d3a2b7768b136f73126e
Balmasexy <www.alabamaabiodun@gmail.com>
2026-07-25 10:24:41 +0100

    WIP: backend progress before environment repair

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
 import { StaffModule } from './staff/staff.module';
 
 @Module({
   imports: [
+    PrismaModule,
     StaffModule,
   ],
 })

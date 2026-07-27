aa05348f46a82e7124a8a4853aac3bb5c3ff82a7
Balmasexy <www.alabamaabiodun@gmail.com>
2026-07-27 09:01:34 +0100

    fix: import AuthModule into AppModule

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { StaffModule } from './staff/staff.module';
 import { ReportsModule } from './reports/reports.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     PrismaModule,
     StaffModule,
     ReportsModule,
+    AuthModule,
   ],
 })
 export class AppModule {}

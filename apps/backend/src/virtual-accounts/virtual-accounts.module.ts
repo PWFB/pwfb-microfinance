342d0cd1c8538ca621dce502e0ac896e8c0806e3
balmasexy <131864138+Balmasexy@users.noreply.github.com>
2026-08-22 19:09:27 +0100

    wire Paystack virtual account provider

--- apps/backend/src/virtual-accounts/virtual-accounts.module.ts
+++ apps/backend/src/virtual-accounts/virtual-accounts.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { VirtualAccountsService } from './virtual-accounts.service';
+import { PaystackVirtualAccountProvider } from './paystack-virtual-account.provider';
+import { VIRTUAL_ACCOUNT_PROVIDER } from './virtual-accounts.types';
 
 @Module({
-  providers: [VirtualAccountsService],
+  providers: [
+    VirtualAccountsService,
+    PaystackVirtualAccountProvider,
+    {
+      provide: VIRTUAL_ACCOUNT_PROVIDER,
+      useExisting: PaystackVirtualAccountProvider,
+    },
+  ],
   exports: [VirtualAccountsService],
 })
 export class VirtualAccountsModule {}

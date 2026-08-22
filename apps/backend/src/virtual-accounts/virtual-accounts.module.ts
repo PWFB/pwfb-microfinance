bb48b3a7407d1462c71733bbe9a6fd89538f883a
balmasexy <131864138+Balmasexy@users.noreply.github.com>
2026-08-22 19:12:14 +0100

    wire prisma into virtual account module

--- apps/backend/src/virtual-accounts/virtual-accounts.module.ts
+++ apps/backend/src/virtual-accounts/virtual-accounts.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
 import { VirtualAccountsService } from './virtual-accounts.service';
 import { PaystackVirtualAccountProvider } from './paystack-virtual-account.provider';
 import { VIRTUAL_ACCOUNT_PROVIDER } from './virtual-accounts.types';
 
 @Module({
+  imports: [PrismaModule],
   providers: [
     VirtualAccountsService,
     PaystackVirtualAccountProvider,

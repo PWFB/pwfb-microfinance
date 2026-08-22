a706ca1288a9cf25a97a99befdb23b6630fbdf02
balmasexy <131864138+Balmasexy@users.noreply.github.com>
2026-08-22 19:54:40 +0100

    register virtual account webhook module

--- apps/backend/src/banking/banking.module.ts
+++ apps/backend/src/banking/banking.module.ts
@@ -2,12 +2,25 @@ import { Module } from '@nestjs/common';
 import { BankingController } from './banking.controller';
 import { BankingService } from './banking.service';
 import { CustomerVirtualAccountService } from './customer-virtual-account.service';
+import { CustomerVirtualAccountWebhookService } from './customer-virtual-account-webhook.service';
+import { CustomerVirtualAccountWebhookController } from './customer-virtual-account-webhook.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [BankingController],
-  providers: [BankingService, CustomerVirtualAccountService],
-  exports: [BankingService, CustomerVirtualAccountService],
+  controllers: [
+    BankingController,
+    CustomerVirtualAccountWebhookController,
+  ],
+  providers: [
+    BankingService,
+    CustomerVirtualAccountService,
+    CustomerVirtualAccountWebhookService,
+  ],
+  exports: [
+    BankingService,
+    CustomerVirtualAccountService,
+    CustomerVirtualAccountWebhookService,
+  ],
 })
 export class BankingModule {}

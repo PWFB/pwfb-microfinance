153d0ac201f0e9682cc00953fab1fa7e719cf23e
balmasexy <131864138+Balmasexy@users.noreply.github.com>
2026-08-22 19:23:35 +0100

    register external bank transfer service

--- apps/backend/src/banking/banking.module.ts
+++ apps/backend/src/banking/banking.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { BankingController } from './banking.controller';
 import { BankingService } from './banking.service';
 import { NibssService } from './nibss.service';
+import { ExternalBankTransferService } from './external-bank-transfer.service';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
   controllers: [BankingController],
-  providers: [BankingService, NibssService],
-  exports: [BankingService, NibssService],
+  providers: [BankingService, NibssService, ExternalBankTransferService],
+  exports: [BankingService, NibssService, ExternalBankTransferService],
 })
 export class BankingModule {}

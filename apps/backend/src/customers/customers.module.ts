95e70649d0b88ef82ed5727cab596617c6e4a0fd
balmasexy <131864138+Balmasexy@users.noreply.github.com>
2026-08-22 19:12:18 +0100

    connect customer creation to virtual accounts

--- apps/backend/src/customers/customers.module.ts
+++ apps/backend/src/customers/customers.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { VirtualAccountsModule } from '../virtual-accounts/virtual-accounts.module';
 import { CustomersController } from './customers.controller';
 import { CustomersService } from './customers.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, VirtualAccountsModule],
   controllers: [CustomersController],
   providers: [CustomersService],
 })

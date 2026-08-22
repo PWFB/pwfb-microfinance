e5ec833473787296dad0112b7468d4043829f282
balmasexy <131864138+Balmasexy@users.noreply.github.com>
2026-08-22 19:50:21 +0100

    Register customer virtual account service

--- apps/backend/src/banking/banking.module.ts
+++ apps/backend/src/banking/banking.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BankingController } from './banking.controller';
 import { BankingService } from './banking.service';
+import { CustomerVirtualAccountService } from './customer-virtual-account.service';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
   controllers: [BankingController],
-  providers: [BankingService],
-  exports: [BankingService],
+  providers: [BankingService, CustomerVirtualAccountService],
+  exports: [BankingService, CustomerVirtualAccountService],
 })
 export class BankingModule {}

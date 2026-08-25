5a085b7e8f332a1890c59a75cef33a0fe449a46e
balmasexy <131864138+Balmasexy@users.noreply.github.com>
2026-08-25 02:08:31 +0100

    accept customer passport during registration

--- apps/backend/src/auth/dto/register.dto.ts
+++ apps/backend/src/auth/dto/register.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class RegisterDto {
   @IsEmail()
@@ -16,4 +16,8 @@ export class RegisterDto {
 
   @IsString()
   phone: string;
+
+  @IsOptional()
+  @IsString()
+  passportPhoto?: string;
 }

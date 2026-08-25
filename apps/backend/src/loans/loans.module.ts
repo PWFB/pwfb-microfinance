6eb48afc8a9791e3e12a2a0fc23d7f750801a60d
balmasexy <131864138+Balmasexy@users.noreply.github.com>
2026-08-25 04:10:46 +0100

    Wire banking providers into loan disbursement workflow

--- apps/backend/src/loans/loans.module.ts
+++ apps/backend/src/loans/loans.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { BankingModule } from '../banking/banking.module';
 import { LoansController } from './loans.controller';
 import { LoansService } from './loans.service';
+import { LoanDisbursementService } from './loan-disbursement.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, BankingModule],
   controllers: [LoansController],
-  providers: [LoansService],
-  exports: [LoansService],
+  providers: [LoansService, LoanDisbursementService],
+  exports: [LoansService, LoanDisbursementService],
 })
 export class LoansModule {}

9208df0a71772c8f1fa32707d3fd3c5bdb7b9f17
balmasexy <131864138+Balmasexy@users.noreply.github.com>
2026-08-25 03:16:21 +0100

    register organization hierarchy module

--- apps/backend/src/staff/staff.module.ts
+++ apps/backend/src/staff/staff.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { StaffController } from './staff.controller';
 import { StaffService } from './staff.service';
 import { StaffRepository } from './staff.repository';
+import { OrganizationController } from './organization.controller';
+import { OrganizationService } from './organization.service';
 import { BankingModule } from '../banking/banking.module';
 
 @Module({
   imports: [BankingModule],
-  controllers: [StaffController],
-  providers: [StaffService, StaffRepository],
-  exports: [StaffService, StaffRepository],
+  controllers: [StaffController, OrganizationController],
+  providers: [StaffService, StaffRepository, OrganizationService],
+  exports: [StaffService, StaffRepository, OrganizationService],
 })
 export class StaffModule {}

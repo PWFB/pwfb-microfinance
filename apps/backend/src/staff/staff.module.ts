b871c659b3c096b8c126dfeb619178e12c6c4bb8
balmasexy <131864138+Balmasexy@users.noreply.github.com>
2026-08-25 02:14:35 +0100

    connect staff registration to Flutterwave BVN service

--- apps/backend/src/staff/staff.module.ts
+++ apps/backend/src/staff/staff.module.ts
@@ -2,18 +2,12 @@ import { Module } from '@nestjs/common';
 import { StaffController } from './staff.controller';
 import { StaffService } from './staff.service';
 import { StaffRepository } from './staff.repository';
+import { BankingModule } from '../banking/banking.module';
 
 @Module({
-  controllers: [
-    StaffController,
-  ],
-  providers: [
-    StaffService,
-    StaffRepository,
-  ],
-  exports: [
-    StaffService,
-    StaffRepository,
-  ],
+  imports: [BankingModule],
+  controllers: [StaffController],
+  providers: [StaffService, StaffRepository],
+  exports: [StaffService, StaffRepository],
 })
 export class StaffModule {}

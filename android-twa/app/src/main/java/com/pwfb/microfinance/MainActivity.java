2a1fdd989643107dec9e026df88e71f77360fd0f
balmasexy <131864138+Balmasexy@users.noreply.github.com>
2026-09-01 04:39:50 +0100

    Add refresh button to PWFB Android login

--- android-twa/app/src/main/java/com/pwfb/microfinance/MainActivity.java
+++ android-twa/app/src/main/java/com/pwfb/microfinance/MainActivity.java
@@ -1,9 +1,12 @@
 package com.pwfb.microfinance;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.browser.customtabs.CustomTabsIntent;
 
 public class MainActivity extends Activity {
     private static final String START_URL = "https://pwfb-frontend.onrender.com/";
@@ -11,14 +14,25 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        openPwfbInBrowser();
+        openPwfb();
     }
 
-    private void openPwfbInBrowser() {
-        Uri uri = Uri.parse(START_URL);
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-        finish();
+    private void openPwfb() {
+        Bitmap refreshIcon = BitmapFactory.decodeResource(
+                getResources(), android.R.drawable.ic_popup_sync
+        );
+
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        if (refreshIcon != null) {
+            builder.setActionButton(
+                    refreshIcon,
+                    "Refresh PWFB login page",
+                    RefreshReceiver.createPendingIntent(this),
+                    true
+            );
+        }
+
+        CustomTabsIntent customTabsIntent = builder.build();
+        customTabsIntent.launchUrl(this, Uri.parse(START_URL));
     }
 }

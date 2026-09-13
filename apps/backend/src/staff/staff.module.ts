f275105822d3a986e4f0fd82a5eddffdac58d63d
balmasexy <131864138+Balmasexy@users.noreply.github.com>
2026-09-13 11:19:30 +0100

    feat(bvn): register Flutterwave staff webhook controller

--- apps/backend/src/staff/staff.module.ts
+++ apps/backend/src/staff/staff.module.ts
@@ -4,11 +4,12 @@ import { StaffService } from './staff.service';
 import { StaffRepository } from './staff.repository';
 import { OrganizationController } from './organization.controller';
 import { OrganizationService } from './organization.service';
+import { StaffBvnWebhookController } from './staff-bvn-webhook.controller';
 import { BankingModule } from '../banking/banking.module';
 
 @Module({
   imports: [BankingModule],
-  controllers: [StaffController, OrganizationController],
+  controllers: [StaffController, OrganizationController, StaffBvnWebhookController],
   providers: [StaffService, StaffRepository, OrganizationService],
   exports: [StaffService, StaffRepository, OrganizationService],
 })

cea6926366087aedc122569490f092aa13794fda
balmasexy <131864138+Balmasexy@users.noreply.github.com>
2026-09-14 21:25:29 +0100

    feat(finance): connect field collections to staff wallets

--- apps/backend/src/collections/collections.module.ts
+++ apps/backend/src/collections/collections.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CollectionsController } from './collections.controller';
 import { CollectionsService } from './collections.service';
+import { StaffWalletModule } from '../staff-wallet/staff-wallet.module';
 
 @Module({
+  imports: [StaffWalletModule],
   controllers: [CollectionsController],
-  providers: [CollectionsService]
+  providers: [CollectionsService],
 })
 export class CollectionsModule {}

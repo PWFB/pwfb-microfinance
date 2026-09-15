b67abc7f3a5dc04d4397d1f618b05c714a2d7ff3
balmasexy <131864138+Balmasexy@users.noreply.github.com>
2026-09-15 09:58:02 +0100

    feat(permissions): wire loan permission enforcement

--- apps/backend/src/loans/loans.module.ts
+++ apps/backend/src/loans/loans.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { BankingModule } from '../banking/banking.module';
+import { PermissionsModule } from '../permissions/permissions.module';
 import { LoansController } from './loans.controller';
 import { LoansService } from './loans.service';
 import { LoanDisbursementService } from './loan-disbursement.service';
 
 @Module({
-  imports: [PrismaModule, BankingModule],
+  imports: [PrismaModule, BankingModule, PermissionsModule],
   controllers: [LoansController],
   providers: [LoansService, LoanDisbursementService],
   exports: [LoansService, LoanDisbursementService],

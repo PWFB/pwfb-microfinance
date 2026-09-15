b8d8b283759f73efc8825348d00b8de76c015e1f
balmasexy <131864138+Balmasexy@users.noreply.github.com>
2026-09-15 10:05:37 +0100

    fix(savings): provide permissions service to savings controller

--- apps/backend/src/savings/savings.module.ts
+++ apps/backend/src/savings/savings.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { PermissionsModule } from '../permissions/permissions.module';
 import { SavingsController } from './savings.controller';
 import { SavingsService } from './savings.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PermissionsModule],
   controllers: [SavingsController],
   providers: [SavingsService],
   exports: [SavingsService],
